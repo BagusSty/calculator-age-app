2c94d0c0121dab6bedcc823f7dec0cfd4cb20548
Bagus Setyo <bagussetyo442@gmail.com>
2023-11-08 19:15:38 +0700

    function calculate

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,98 @@
 <script>
 export default {
+  name: "AgeCalc",
   data() {
     return {
-      inputs: [
-        {
-          label: "DAY",
-          placeholder: "DD",
-          vModel: "day",
-        },
-        {
-          label: "MONTH",
-          placeholder: "MM",
-          vModel: "month",
-        },
-        {
-          label: "YEAR",
-          placeholder: "YYYY",
-          vModel: "year",
-        },
-      ],
-      age: [
-        {
-          text: "years",
-        },
-        {
-          text: "months",
-        },
-        {
-          text: "days",
-        },
-      ],
-      results: [
-        {
-          day: "",
-          month: "",
-          year: "",
-        },
-      ],
+      dateResult: false,
+      pastYear: true,
+      validDay: true,
+      validMonth: true,
+      emptyInput: false,
+      years: 0,
+      months: 0,
+      days: 0,
+      day: null,
+      month: null,
+      year: null,
     };
   },
   methods: {
-    toggleAnswer(index) {
-      this.faqs[index].showAnswer = !this.faqs[index].showAnswer;
+    calculateAge() {
+      const day = this.day;
+      const month = this.month;
+      const year = this.year;
+
+      const date = new Date();
+      const currentYear = date.getFullYear();
+      const currentMonth = date.getMonth() + 1;
+      const currentDay = date.getDate();
+
+      let years = 0;
+      if (
+        currentMonth > month ||
+        (currentMonth == month && currentDay >= day)
+      ) {
+        years = currentYear - year;
+      } else {
+        years = currentYear - year - 1;
+      }
+
+      let months = 0;
+      if (currentDay >= day) {
+        months = currentMonth - month;
+      } else if (currentDay < day) {
+        months = currentMonth - month - 1;
+      }
+
+      months = months < 0 ? months + 12 : months;
+
+      let days = 0;
+      let monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+      if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        monthDays[1] = 29;
+      }
+
+      if (currentDay >= day) {
+        days = currentDay - day;
+      } else if (currentDay < day) {
+        days = currentDay - day + monthDays[month - 1];
+      }
+
+      if (day > monthDays[month - 1]) {
+        this.validDay = false;
+        this.dateResult = false;
+      } else {
+        this.validDay = true;
+      }
+
+      if (month > 12) {
+        this.validMonth = false;
+        this.dateResult = false;
+      } else {
+        this.validMonth = true;
+      }
+
+      if (years < 0) {
+        this.dateResult = false;
+        this.pastYear = false;
+      } else if (day === null || month === null || year === null) {
+        this.dateResult = false;
+        this.emptyInput = true;
+      } else if (
+        this.validDay == false ||
+        this.validMonth == false ||
+        this.validYear == false
+      ) {
+        this.dateResult = false;
+      } else {
+        this.dateResult = true;
+        this.pastYear = true;
+        this.emptyInput = false;
+        this.years = years;
+        this.months = months;
+        this.days = days;
+      }
     },
   },
 };
@@ -53,40 +104,118 @@ export default {
         class="max-w-2xl w-80 sm:w-full mx-auto m-4 bg-white shadow-lg rounded-t-2xl rounded-bl-2xl custom-rounded-br sm:p-12 p-8 text-center justify-center items-center"
       >
         <div class="flex">
-          <div class="m-2" v-for="(input, index) in inputs" :key="index">
+          <div class="m-2">
             <label
               for="day"
               class="block text-left font-bold mb-2 text-lightGrey"
-              >{{ input.label }}</label
+              :class="{ 'text-red-300': !validDay || emptyInput }"
+              >DAY</label
             >
             <input
               class="text-semibold placeholder:text-lightGrey sm:p-3 p-2 sm:w-40 w-full border-lightGrey border cursor-pointer rounded-md hover:border-customPurple remove-arrow"
+              :class="{ 'border-red-500': !validDay || emptyInput }"
               type="number"
-              v-model="input.vModel"
-              :placeholder="input.placeholder"
-              :id="input.label"
-              :name="input.label"
+              v-model="day"
+              placeholder="DD"
+              id="day"
+              name="day"
+              required:true
             />
+            <hr />
+            <span class="error italic text-red-500 font-thin" v-if="emptyInput"
+              >This field is required</span
+            >
+            <span
+              class="error italic text-red-500 font-thin"
+              v-else-if="!validDay"
+              >Must be a valid date</span
+            >
+          </div>
+          <div class="m-2">
+            <label
+              for="day"
+              class="block text-left font-bold mb-2 text-lightGrey"
+              :class="{ 'text-red-300': !validMonth || emptyInput }"
+              >MONTH</label
+            >
+            <input
+              class="text-semibold placeholder:text-lightGrey sm:p-3 p-2 sm:w-40 w-full border-lightGrey border cursor-pointer rounded-md hover:border-customPurple remove-arrow"
+              :class="{ 'border-red-500': !validMonth || emptyInput }"
+              type="number"
+              v-model="month"
+              placeholder="MM"
+              id="month"
+              name="month"
+              required:true
+            />
+            <hr />
+            <span class="error italic text-red-500 font-thin" v-if="emptyInput"
+              >This field is required</span
+            >
+            <span
+              class="error italic text-red-500 font-thin"
+              v-else-if="!validMonth"
+              >Must be a valid month</span
+            >
+          </div>
+          <div class="m-2">
+            <label
+              for="day"
+              class="block text-left font-bold mb-2 text-lightGrey"
+              :class="{ 'text-red-300': !pastYear || emptyInput }"
+              >YEAR</label
+            >
+            <input
+              class="text-semibold placeholder:text-lightGrey sm:p-3 p-2 sm:w-40 w-full border-lightGrey border cursor-pointer rounded-md hover:border-customPurple remove-arrow"
+              :class="{ 'border-red-500': !pastYear || emptyInput }"
+              type="number"
+              v-model="year"
+              placeholder="YYYY"
+              id="year"
+              name="year"
+              required:true
+            />
+            <hr />
+            <span class="error italic text-red-500 font-thin" v-if="emptyInput"
+              >This field is required</span
+            >
+            <span
+              class="error italic text-red-500 font-thin"
+              v-else-if="!pastYear"
+              >Must be a valid year</span
+            >
           </div>
         </div>
         <div class="flex items-center justify-center">
           <hr class="border-t border-gray-300 flex-grow" />
-          <button class="bg-customPurple p-2 rounded-full hover:bg-offBlack">
+          <button
+            class="bg-customPurple p-2 rounded-full hover:bg-offBlack"
+            @click="calculateAge()"
+          >
             <img src="/assets/images/icon-arrow.svg" alt="" />
           </button>
         </div>
         <div class="mt-3 sm:text-7xl text-5xl">
-          <div class="flex m-auto" v-for="(ages, index) in age" :key="index">
-            <h1 class="text-customPurple font-bold">--</h1>
-            <h1 class="text-offBlack font-bold italic">{{ ages.text }}</h1>
+          <div class="flex m-auto">
+            <h1 class="text-customPurple font-bold" v-if="!dateResult">--</h1>
+            <h1 class="text-customPurple font-bold" v-else>{{ years }}</h1>
+            <h1 class="text-offBlack font-bold italic">years</h1>
+          </div>
+          <div class="flex m-auto">
+            <h1 class="text-customPurple font-bold" v-if="!dateResult">--</h1>
+            <h1 class="text-customPurple font-bold" v-else>{{ month }}</h1>
+            <h1 class="text-offBlack font-bold italic">months</h1>
+          </div>
+          <div class="flex m-auto">
+            <h1 class="text-customPurple font-bold" v-if="!dateResult">--</h1>
+            <h1 class="text-customPurple font-bold" v-else>{{ days }}</h1>
+            <h1 class="text-offBlack font-bold italic">days</h1>
           </div>
         </div>
       </div>
     </div>
   </main>
 </template>
-
-<script setup></script>
 
 <style scoped>
 .custom-rounded-br {
